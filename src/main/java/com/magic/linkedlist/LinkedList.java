ff1c00c3e9ee8df940a05551bbe7d5692beff5c2
priyanshi1208 <pri.shukla2208@gmail.com>
2021-06-06 12:46:43 +0530

    ADDED:new method to append the linkedlist

--- src/main/java/com/magic/linkedlist/LinkedList.java
+++ src/main/java/com/magic/linkedlist/LinkedList.java
@@ -7,4 +7,15 @@ public class LinkedList<E> {
         newNode.next=head;
         head=newNode;
     }
+    public void append(E data){
+        Node<E> lastNode=new Node<>(data);
+        if(head==null)
+            head=lastNode;
+        else{
+            Node<E> temp=head;
+            while(temp.next!=null)
+                temp=temp.next;
+            temp.next=lastNode;
+        }
+    }
 }
--- src/test/java/com/magic/linkedlist/LinkedListTest.java
+++ src/test/java/com/magic/linkedlist/LinkedListTest.java
@@ -11,6 +11,14 @@ public class LinkedListTest {
        list.push(30);
        boolean  result = list.head.getData().equals(30);
         Assert.assertTrue(result);
-
+    }
+    @Test
+    public void validating_if_linkedlist_can_push_from_back() {
+        LinkedList<Integer> list=new LinkedList<>();
+        list.push(30);
+        list.append(20);
+        list.append(10);
+        boolean  result = list.head.getData().equals(30)&&list.head.getNext().getData().equals(20)&&list.head.getNext().getNext().getData().equals(10);
+        Assert.assertTrue(result);
     }
 }
